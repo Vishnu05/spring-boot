74946f9681b0cb139c737a1e90456e09c6cd23a5
= <vishnuthamiz96@gmail.com>
2020-04-08 13:31:24 +0530

    initial commit

---
+++ src/test/java/com/vishnu/firstspringbootapp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.firstspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
